10bf49b9bff628faadc271aaf1b575780f4d3c30
TH <tylerhartland7@gmail.com>
2013-07-15 11:17:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TinyUrl::Application.config.secret_key_base = '082fe43b82c3865ae3d2449bc3abc1bb2ae999c2de3c164554ba52d0369d5adeb0de4cddbf7a5bb69a29e0588bfa9a55cb70873ca5357bdf0f8a1bed032f0fa8'
